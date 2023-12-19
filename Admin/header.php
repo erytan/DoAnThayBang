00b3400800b186eb648df0e52b7cc0372238c586
khanh228ey <khanh2282002dzz@gmail.com>
2023-12-19 21:17:43 +0700

    fix

--- Admin/header.php
+++ Admin/header.php
@@ -1,31 +1,4 @@
-<?php
-ob_start();
-?>
-<header class="main-header">
-        <!-- Logo -->
-        <a href="index.php" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>Quản Lí K-Shop</b>S</span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Quản Lí K-Shop</b></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-          </a>
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-           
-              <!-- Notifications: style can be found in dropdown.less -->
-         
-              <!-- Tasks: style can be found in dropdown.less -->
-          
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
                   <img src="../img/avt.jpg" class="user-image" alt="User Image">
                   <span class="hidden-xs">Admin</span>
                 </a>
